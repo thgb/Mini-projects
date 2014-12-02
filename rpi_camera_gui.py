eca87a1a96ad85ae656864e03ccb54cad17e71a9
Tom Bennett <t@thgb.me>
2014-12-02 20:04:00 +0000

    Fixed PEP8 styling

--- rpi_camera_gui.py
+++ rpi_camera_gui.py
@@ -5,17 +5,18 @@ import RPi.GPIO as GPIO
 
 
 camera = picamera.PiCamera()
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 frame = 1
+
 
 def onclick():
     pass
 
+
 def close_window():
     root.destroy()
+
 
 def take_pic():
     global frame
@@ -25,19 +26,19 @@ def take_pic():
     frame += 1
     camera.stop_preview()
 
+
 def detect_button():
-	while True:
-		input_state = GPIO.input(18)
-		if input_state == False:
-			take_pic()
-			break
-			
+    while True:
+        input_state = GPIO.input(18)
+        if input_state is False:
+            take_pic()
+            break
 
 root = Tk()
 root.wm_title("Camera App")
 root.geometry('300x300')
-a = Button(root, background='green', text="Take Pic", height=5, command=take_pic, font=(30))
-b = Button(root, background='green', text="Use Button", height=5, command=detect_button, font=(30))
+a = Button(root, text="Take Pic", height=5, command=take_pic, font=(30))
+b = Button(root, text="Use Button", height=5, command=detect_button, font=(30))
 c = Button(root, text="Done", height=5, command=close_window)
 a.pack()
 b.pack()
